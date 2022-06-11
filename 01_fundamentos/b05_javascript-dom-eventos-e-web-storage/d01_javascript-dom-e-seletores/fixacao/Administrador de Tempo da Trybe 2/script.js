f144591fc8c8812508c550dda82c791698b134f0
Samara Bastos <samarasampaio30@gmail.com>
2022-06-10 23:10:02 -0300

    exercicios

---
+++ 01_fundamentos/b05_javascript-dom-eventos-e-web-storage/d01_javascript-dom-e-seletores/fixacao/Administrador de Tempo da Trybe 2/script.js
@@ -0,0 +1,12 @@
+document.getElementById("header-container").style.background = "green"
+
+document.getElementsByClassName('emergency-tasks')[0].style.background = "pink"
+document.getElementsByTagName('h3')[0].style.background = '#BA55D3'
+document.getElementsByTagName('h3')[1].style.background = '#BA55D3'
+
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.background = 'black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.background = 'black';
+document.querySelectorAll('.no-emergency-tasks')[0].style.background = "#FFD700"
+
+
+document.getElementById("footer-container").style.background = "rgb(0, 53, 51)"
